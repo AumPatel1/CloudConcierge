22a2b2a2887413db4236496f80c78aba743b03d6
Sakoondeep01 <152828114+Sakoondeep01@users.noreply.github.com>
2025-06-23 07:10:53 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
 const salesRoutes = require('./routes/sales');
-const predictionRoute = require('./routes/predict_sales');
+const predictionRoutes = require('./routes/prediction');
 const app = express();
 
 // CORS configuration
@@ -13,7 +13,7 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
 };
 
-// Use CORS middleware
+// Use CORS middlewar
 app.use(cors(corsOptions));
 
 // Middleware to parse JSON requests
@@ -22,16 +22,17 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', authRoutes);  // Authentication routes
 app.use('/api/sales', salesRoutes); // Sales routes
-app.use('/api/predict', predictionRoute);
+app.use('/api/predict', predictionRoutes);
 
 
-mongoose.connect('mongodb+srv://CloudConcierge:GeekSquad@cloudconcierge.tebviqc.mongodb.net/cloudconciergeDB?retryWrites=true&w=majority&tls=true')
-  .then(() => console.log('Connected to MongoDB'))
+mongoose.connect(
+  'mongodb+srv://CloudConcierge:GeekSquad@cloudconcierge.ezpvr.mongodb.net/cloudconcierge?retryWrites=true&w=majority&appName=CloudConcierge'
+)
+  .then(() => console.log(' Connected to MongoDB'))
   .catch((err) => {
     console.error('Failed to connect to MongoDB:', err.message);
     console.error('Full error:', err);
   });
-
 
 // Start the server
 app.listen(5000, () => {
